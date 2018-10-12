084e32177c0d64569c55c5a51da1db4ec96feb0b
iamxiaoma <395487290@qq.com>
2018-10-12 16:13:40 +0800

    初始化 iOS 项目

---
+++ tvaf_iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tvaf_iOS
+//
+//  Created by kk on 2018/10/12.
+//  Copyright © 2018年 framework7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tvaf_iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tvaf_iOS
+//
+//  Created by kk on 2018/10/12.
+//  Copyright © 2018年 framework7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
